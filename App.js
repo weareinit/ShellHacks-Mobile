323468487d65a74092840e3e7ca82f0c9451bf30
aalexcomas11 <aalexcomas11@github.com>
2018-06-28 08:52:27 -0400

    Worked on MenuBar/Interface

--- App.js
+++ App.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  StatusBar
 } from 'react-native';
+
+import Interface from './src/Components/MainPages/Interface/Interface.js'
+import AppMenu from './AppMenuTest.js'
+import Landing from './src/Components/wlecome/Landing_components/Landing.js';
+import Menu from './src/Components/MainPages/Interface/MenuBar/menuBar.js'
 
 export default class App extends Component {
   render() {
     return (
+      //Change color of status bar to white
       <View style={styles.container}>
-
+        {/*<StatusBar barStyle="light-content"/>*/}
+        {/*<Interface style={styles.container} />*/}
+        {/*<AppMenu style={{backgroundColor: 'blue'}}/>*/}
+        <Menu />
       </View>
     );
   }
